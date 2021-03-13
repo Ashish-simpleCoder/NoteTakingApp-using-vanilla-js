c58b46e33132dd9d3ab62338e1798b07ff551d99
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-03-13 20:28:08 +0530

    initial commit

--- 2_effect_script.js
+++ 2_effect_script.js
@@ -6,17 +6,17 @@ const displayNoteOverlay = document.querySelectorAll('.displayNoteOverlay');
 // console.log(r);
 
 const textarea = document.querySelectorAll('textarea');
-window.addEventListener('load', () => {
-   hero_header.classList.toggle('hero_header-active');
-   newNoteBtn.classList.toggle('newNoteBtn-active');
+// window.addEventListener('load', () => {
+//    hero_header.classList.toggle('hero_header-active');
+//    newNoteBtn.classList.toggle('newNoteBtn-active');
 
-   for (i = 0; i < result.length; i = i + 2) {
-      result[i].classList.toggle('result-active_odd');
-   }
-   for (i = 1; i < result.length; i = i + 2) {
-      result[i].classList.toggle('result-active_even');
-   }
-});
+//    for (i = 0; i < result.length; i = i + 2) {
+//       result[i].classList.toggle('result-active_odd');
+//    }
+//    // for (i = 1; i < result.length; i = i + 2) {
+//    //    result[i].classList.toggle('result-active_even');
+//    // }
+// });
 
 // for(i=0;i<textarea.length;i++){
 //     if(textarea[i]focusWithin){
@@ -24,3 +24,23 @@ window.addEventListener('load', () => {
 //         console.log('true')
 //     }
 // }
+
+
+/* ---------------------------------------------- */
+window.addEventListener('load',()=>{
+   const mainContent=document.querySelector('.mainContent')
+   const childrens=mainContent.children;
+   // console.log(resultContent.children[1])
+
+   for(let i=0;i<childrens.length;i++){
+      childrens[i].style.animation=`loading ${1+i/childrens.length}s ease forwards`;
+   }
+})
+window.addEventListener('load',()=>{
+   for(let i=0;i<resultContent.children.length;i++)
+   {
+      resultContent.children[i].style.animation=`loading ${1+(i+0.1/resultContent.children.length)}s ease forwards`;
+   }
+})
+/* ---------------------------------------------- */
+
